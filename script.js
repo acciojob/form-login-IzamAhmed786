4b61094bd0ea259f50f4f754d8b5bc983cd873ff
IzamAhmed786 <izamahmed786@gmail.com>
2025-09-02 10:57:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	      event.preventDefault();
+let firstName = document.forms["nameForm"]["fname"].value.trim();
+      let lastName = document.forms["nameForm"]["lname"].value.trim();
+	      let fullName = firstName + " " + lastName;
+ if (!firstName && !lastName) {
+        alert("Please enter your first name and last name.");
+        return;
+      } else if (!firstName) {
+        alert("Please enter your first name.");
+        return;
+      } else if (!lastName) {
+        alert("Please enter your last name.");
+        return;
+      }
+	      alert(fullName);
 
 }
